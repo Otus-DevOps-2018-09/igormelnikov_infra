076039af8fabf10c3f3ad539b5968c6980f5ef6b
Igor Melnikov <prodwiththeprod@gmail.com>
2018-10-29 01:19:15 +0300

    Added load balancer w/ 2nd instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,6 +45,41 @@ resource "google_compute_instance" "app" {
   }
 }
 
+resource "google_compute_instance" "app2" {
+  name         = "reddit-app2"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+    }
+  }
+
+  tags = ["reddit-app"]
+
+  network_interface {
+    network       = "default"
+    access_config = {}
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+}
+
 resource "google_compute_firewall" "firewall_puma" {
   name    = "allow-puma-default"
   network = "default"
