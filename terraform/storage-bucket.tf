9dd8fc26b6349b12163e0c3000e5b4a95b79b316
Igor Melnikov <prodwiththeprod@gmail.com>
2018-11-01 23:49:49 +0300

    Added bucket backend

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -7,7 +7,7 @@ provider "google" {
 module "storage-bucket" {
   source  = "SweetOps/storage-bucket/google"
   version = "0.1.1"
-  name    = ["reddit-bucket1", "reddit-bucket2"]
+  name    = ["reddit-state-stage", "reddit-state-prod"]
 }
 
 output storage-bucket_url {
